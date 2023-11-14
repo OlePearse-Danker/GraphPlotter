b15d0ff8c4cc2f3b6a8e30546116a09ebd0c011d
Ole Pearse-Danker <ole.pd@icloud.com>
2023-11-14 19:49:40 +0100

    added possibility of function input by user

--- src/GraphPlotter.java
+++ src/GraphPlotter.java
@@ -1,14 +1,18 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
-import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 @SuppressWarnings("serial")
-public class GraphPlotter extends JPanel{
-    private Graphics g;
+public class GraphPlotter extends JPanel {
     private JButton zoomInButton;
     private JButton zoomOutButton;
 
@@ -17,8 +21,10 @@ public class GraphPlotter extends JPanel{
 
     private double xMin, yMin, xMax, yMax;
     private double xUnit, yRange;
-    private double xRange; // Added variable declaration
-    private double yUnit; // Added variable declaration
+    private double xRange;
+    private double yUnit;
+
+    private String expression; // User-entered function expression
 
     public GraphPlotter(int screenMaxX, int screenMaxY) {
         this.screenMaxX = screenMaxX;
@@ -38,102 +44,89 @@ public class GraphPlotter extends JPanel{
         createZoomInButton();
         createZoomOutButton();
 
+        // Prompt the user to enter the function expression
+        expression = JOptionPane.showInputDialog("Enter the function expression:");
+        if (expression == null) {
+            expression = ""; // Set empty string if user cancels input
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        this.g = g;
+        Graphics2D g2d = (Graphics2D) g;
 
+        coordinateSystem(g2d);
+        plot(g2d);
+    }
+
+    private void drawPoint(Graphics2D g2d, Point p, Color c) {
+        g2d.setColor(c);
+        g2d.drawLine(p.x, p.y, p.x, p.y);
+    }
+
+    private void coordinateSystem(Graphics2D g2d) {
         int screenWidth = getWidth();
         int screenHeight = getHeight();
 
-        screenMaxX = screenWidth;
-        screenMaxY = screenHeight;
-
-        xUnit = xRange / screenMaxX;
-        yUnit = yRange / screenMaxY;
-
-        coordinateSystem();
-        plot();
-    }
-
-    private void drawPoint(Point p, Color c) {
-        g.setColor(c);
-        g.drawLine(p.x, p.y, p.x, p.y);
-    }
-
-    private void coordinateSystem() {
-        int screenWidth = getWidth();
-        int screenHeight = getHeight();
-
-        g.clearRect(0, 0, screenWidth, screenHeight); // Clear the screen
+        g2d.clearRect(0, 0, screenWidth, screenHeight); // Clear the screen
 
         int gridSize = 50; // Adjust this value to change the grid spacing
 
         // Draw the grid lines
-        g.setColor(Color.LIGHT_GRAY);
+        g2d.setColor(Color.LIGHT_GRAY);
         for (int xScreen = 0; xScreen <= screenWidth; xScreen += gridSize) {
-            g.drawLine(xScreen, 0, xScreen, screenHeight);
+            g2d.drawLine(xScreen, 0, xScreen, screenHeight);
         }
         for (int yScreen = 0; yScreen <= screenHeight; yScreen += gridSize) {
-            g.drawLine(0, yScreen, screenWidth, yScreen);
+            g2d.drawLine(0, yScreen, screenWidth, yScreen);
         }
 
         // Draw the x-axis
-        g.setColor(Color.BLACK);
-        g.drawLine(0, screenHeight / 2, screenWidth, screenHeight / 2);
+        g2d.setColor(Color.BLACK);
+        g2d.drawLine(0, screenHeight / 2, screenWidth, screenHeight / 2);
 
         // Draw the y-axis
-        g.drawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight);
+        g2d.drawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight);
 
         // Add labels for x-axis
         for (int xScreen = 0; xScreen < screenWidth; xScreen += gridSize) {
             double xValue = xMin + (xScreen * xUnit);
-            g.drawString(String.format("%.2f", xValue), xScreen, screenHeight / 2 + 15);
+            g2d.drawString(String.format("%.2f", xValue), xScreen, screenHeight / 2 + 15);
         }
 
         // Add labels for y-axis
         for (int yScreen = 0; yScreen < screenHeight; yScreen += gridSize) {
             double yValue = yMax - ((double) yScreen / screenHeight) * yRange;
-            g.drawString(String.format("%.2f", yValue), screenWidth / 2 + 5, yScreen + 15);
+            g2d.drawString(String.format("%.2f", yValue), screenWidth / 2 + 5, yScreen + 15);
         }
 
         // Draw y = 0 line
-        g.setColor(Color.RED);
+        g2d.setColor(Color.RED);
         int yScreenZero = screenHeight / 2;
-        g.drawLine(0, yScreenZero, screenWidth, yScreenZero);
+        g2d.drawLine(0, yScreenZero, screenWidth, yScreenZero);
 
         // Draw x = 0 line
-        g.drawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight);
+        g2d.drawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight);
     }
 
-
-
-
-
-
-    private void plot() {
+    private void plot(Graphics2D g2d) {
         double x = xMin;
-        for (int xScreen = 0; xScreen < screenMaxX; xScreen++) {
-            x = x + xUnit;
-            int yScreen = scrrenMidY() - ((int) ((f(x) / yRange) * screenMaxY));
-            drawPoint(new Point(xScreen, yScreen), Color.BLUE);
+        while (x <= xMax) {
+            int xScreen = (int) ((x - xMin) / xUnit);
+            int yScreen = (int) (screenMaxY - (f(x) - yMin) / yUnit);
+            drawPoint(g2d, new Point(xScreen, yScreen), Color.BLUE);
+
+            x += xUnit;
         }
-
-        // Zeichne die Linie für y = 0
-//        g.setColor(Color.RED);
-//        int yScreenZero = scrrenMidY() - ((int) ((f(0.0) / yRange) * screenMaxY));
-//        g.drawLine(0, yScreenZero, screenMaxX, yScreenZero);
-
     }
+
     private void createZoomInButton() {
         zoomInButton = new JButton("Zoom In");
         zoomInButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Call the zoom-in method here
-                zoomIn(); // Call the zoom-in method
+                zoomIn();
             }
         });
         add(zoomInButton);
@@ -144,51 +137,77 @@ public class GraphPlotter extends JPanel{
         zoomOutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Call the zoom-out method here
-                zoomOut(); // Call the zoom-out method
+                zoomOut();
             }
         });
         add(zoomOutButton);
     }
 
     private void zoomIn() {
-        // Decrease the x-axis and y-axis ranges for zooming in
         double zoomFactor = 2.0;
         xRange /= zoomFactor;
         yRange /= zoomFactor;
 
-        // Recalculate the xUnit and yUnit values based on the updated ranges
         xUnit = xRange / screenMaxX;
         yUnit = yRange / screenMaxY;
 
-        // Repaint the panel to reflect the new zoom level
         repaint();
     }
 
     private void zoomOut() {
-        // Increase the x-axis and y-axis ranges for zooming out
         double zoomFactor = 2.0;
         xRange *= zoomFactor;
         yRange *= zoomFactor;
 
-        // Recalculate the xUnit and yUnit values based on the updated ranges
         xUnit = xRange / screenMaxX;
         yUnit = yRange / screenMaxY;
 
-        // Repaint the panel to reflect the new zoom level
         repaint();
     }
 
-    private int scrrenMidX() {
+    private int screenMidX() {
         return screenMaxX / 2;
     }
 
-    private int scrrenMidY() {
+    private int screenMidY() {
         return screenMaxY / 2;
     }
 
     private double f(double x) {
-        return Math.sin(x);
-        //return Math.exp(x);
+        try {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+            // Replace 'x' with the current x value and evaluate the expression
+            engine.put("x", x);
+            Object result = engine.eval(expression);
+            return Double.parseDouble(result.toString());
+
+
+        } catch (Exception e) {
+            return 0.0; // Return 0 if the expression is invalid or the evaluation fails
+        }
     }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
+        GraphPlotter plotter = new GraphPlotter(800, 600);
+        javax.swing.JFrame frame = new javax.swing.JFrame("Graph Plotter");
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+
+        // Set the preferred size of the JPanel
+        plotter.setPreferredSize(new java.awt.Dimension(800, 600));
+
+        frame.setContentPane(plotter);
+        frame.pack(); // Adjust the JFrame size based on the preferred size
+        frame.setVisible(true);
+    }
+
 }
